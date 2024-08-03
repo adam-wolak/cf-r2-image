186f38644697cc09df3395b3994f524ca9622aca
adam-wolak <adam.wolak@gmail.com>
2024-08-03 21:38:51 +0200

    Delete workers directory

--- workers/imageOptimizationPipeline.ts
+++
@@ -1,9 +0,0 @@
-import { Env } from '../types';
-
-export default {
-  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    const url = new URL(request.url);
-    const imageCollectorUrl = `https://${env.IMAGE_COLLECTOR_WORKER}${url.pathname}${url.search}`;
-    return fetch(imageCollectorUrl, request);
-  }
-};
